2a5148a7211ea453a5cf9fd2272fd361f51bed8a
Holger Krupp <email@holgerkrupp.de>
2022-08-13 14:26:16 +0200

    Test App

--- SPM_Test/ViewController.swift
+++ SPM_Test/ViewController.swift
@@ -6,12 +6,38 @@
 //
 
 import UIKit
+#if canImport(GoogleMobileAds)
+    import GoogleMobileAds
+#endif
+
 
 class ViewController: UIViewController {
+    
+#if canImport(GoogleMobileAds)
+    var bannerView = GADBannerView(adSize: GADAdSizeBanner)
+#endif
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func addBanner(){
+#if canImport(GoogleMobileAds)
+
+                GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = ["72446c5f7abac9ec97ab0fd28529fd8b",GADSimulatorID]
+                bannerView.rootViewController = self
+        self.view.addSubview(bannerView)
+
+                NSLayoutConstraint.activate([
+                    bannerView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
+                    bannerView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0),
+                    bannerView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0),
+                    bannerView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0),
+                ]
+                )
+                
+#endif
     }
 
 
