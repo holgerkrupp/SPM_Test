de6f8570e07962e386475f77e28a4028278857ba
Holger Krupp <email@holgerkrupp.de>
2022-08-13 14:19:23 +0200

    Initial Commit

---
+++ SPM_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPM_Test
+//
+//  Created by Holger Krupp on 13.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
